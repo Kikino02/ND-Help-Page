26689d27c3306253d81edca7c8010af3fe5ca4e0
Kiko Gajdoš <kiko.gajdos@gmail.com>
2024-07-29 21:39:05 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import './styles/modern-normalize.css'
+import './styles/styles.css'
+import './styles/components/header.css'
+import './styles/components/help.css'
+import './styles/components/cards.css'
+import './styles/components/footer.css'
+import './styles/utils.css'
+
