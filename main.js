4612df8972458471d77bb11b2de3563a33294ae4
Kiko Gajdoš <kiko.gajdos@gmail.com>
2024-08-01 17:07:45 +0200

    sc

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-import { loadNavbar } from './reusable-components/navbar/navbar.js';
-import { loadFooter } from './reusable-components/footer/footer.js';
-import { inject } from '@vercel/analytics';
-
 import './styles/modern-normalize.css';
 import './styles/styles.css';
 import './styles/components/help.css';
 import './styles/components/cards.css';
 import './styles/utils.css';
+
+import { loadNavbar } from './reusable-components/navbar/navbar.js';
+import { loadFooter } from './reusable-components/footer/footer.js';
+import { inject } from '@vercel/analytics';
+
 
 
 loadNavbar();
